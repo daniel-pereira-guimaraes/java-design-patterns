f6f4feea5395eee2fc31b3f0740f2cb0be070ba5
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2022-12-12 19:39:24 -0400

    Update singleton1.Application class.

--- src/singleton1/Application.java
+++ src/singleton1/Application.java
@@ -3,13 +3,13 @@ package singleton1;
 public class Application {
 
 	public static void main(String[] args) {
+		System.out.println(Application.class.getName());
 		System.out.println("Testing Connection class...");
 		Connection c1 = Connection.getInstance();
 		Connection c2 = Connection.getInstance();
 		Connection c3 = Connection.getInstance();
 		if (c1 == c2 && c2 == c3) 
 			System.out.println("c1 == c2 == c3");
-		System.out.println();
 	}
 	
 }
