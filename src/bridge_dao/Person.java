88bc073e7456b072656edc9df9a85ee8d460df76
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2022-12-15 20:08:02 -0400

    Update Person class on bridge_dao package.

--- src/bridge_dao/Person.java
+++ src/bridge_dao/Person.java
@@ -19,14 +19,6 @@ public class Person {
 		return name;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
 	@Override
 	public String toString() {
 		return "Person [id=" + id + ", name=" + name + "]";
