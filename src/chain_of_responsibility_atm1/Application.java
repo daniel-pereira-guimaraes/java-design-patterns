a61cbc528995e0f1894893c94d47e3b6a6cff8d1
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2022-12-19 20:09:23 -0400

    Update chain of responsibility pattern example - atm1.

--- src/chain_of_responsibility_atm1/Application.java
+++ src/chain_of_responsibility_atm1/Application.java
@@ -5,7 +5,7 @@ public class Application {
 	public static void main(String[] args) {
 
 		Atm atm = new Atm(new Bill(100, new Bill(50, new Bill(10, new Bill(5, new Bill(1))))));
-		atm.withdraw(1534);
+		atm.withdraw(1584);
 	}
 
 }
